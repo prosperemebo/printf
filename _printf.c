69745e8458768a5766de11f7ada08e174d3f3501
Prosper Emebo <emeboprosper@gmail.com>
2023-04-22 23:02:10 +0100

    New Push

--- _printf.c
+++ _printf.c
@@ -94,6 +94,51 @@ int print_int(va_list args)
 }
 
 /**
+ * print_unint - Print unsigned int function to print integer or unsigned int
+ *
+ * @args: va_list containing string to print
+ * Return: Number of printed characters in int
+ */
+int print_unint(va_list args)
+{
+	unsigned int num = va_arg(args, int);
+	int digitCount = 0;
+	int count = 0;
+	int temp, i, j;
+	char *digits;
+
+	if (num < 0)
+	{
+		putchar('-');
+		count++;
+		num = -num;
+	}
+
+	temp = num;
+	while (temp != 0)
+	{
+		digitCount++;
+		temp /= 10;
+	}
+
+	digits = (char *)malloc(sizeof(char) * (digitCount + 1));
+
+	for (i = 0; i <= digitCount; i++)
+	{
+		digits[i] = (char)(num % 10 + '0');
+		num /= 10;
+	}
+
+	for (j = digitCount - 1; j >= 0; j--)
+	{
+		putchar(digits[j]);
+		count++;
+	}
+
+	return (count);
+}
+
+/**
  * _printf - Custom implementation of the printf function
  *
  * @format: Input string containing zero or more directives
@@ -110,6 +155,9 @@ int _printf(const char *format, ...)
 	    {'s', print_string},
 	    {'d', print_int},
 	    {'i', print_int},
+	    {'u', print_unint},
+	    {'o', print_unint},
+	    {'x', print_unint},
 	    {'\0', NULL}};
 	va_start(args, format);
 	while (*format != '\0')
