364bf03b9a6d0e4f2493181cec59a319ec8fb163
Prosper Emebo <emeboprosper@gmail.com>
2023-04-22 22:43:49 +0100

    refactor: made methods into struct

--- _printf.c
+++ _printf.c
@@ -1,6 +1,111 @@
 #include "main.h"
 
 /**
+ * struct print_func - Struct for function pointer to print function
+ *
+ * @type: Format Specifier
+ * @execute: Method to handle format specifier
+ */
+typedef struct print_func
+{
+	char type;
+	int (*execute)(va_list);
+} print_func_t;
+
+/**
+ * print_char - Print Char function to print character
+ *
+ * @args: va_list containing characters to print
+ * Return: Number of printed characters;
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+	putchar(c);
+
+	return (1);
+}
+
+/**
+ * print_string - Print String function to print string
+ *
+ * @args: va_list containing string to print
+ * Return: Number of printed characters in string
+ */
+int print_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		putchar(*s);
+		s++;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * print_percent - Print percent function to print percent
+ *
+ * Return: (1)
+ */
+int print_percent()
+{
+
+	putchar('%');
+
+	return (1);
+}
+
+/**
+ * print_string - Print int function to print integer or unsigned int
+ *
+ * @args: va_list containing string to print
+ * Return: Number of printed characters in int
+ */
+int print_int(va_list args)
+{
+	int num = va_arg(args, int);
+	int digitCount = 0;
+	int count = 0;
+	int temp, i, j;
+	char *digits;
+
+	if (num < 0)
+	{
+		putchar('-');
+		count++;
+		num = -num;
+	}
+
+	temp = num;
+	while (temp != 0)
+	{
+		digitCount++;
+		temp /= 10;
+	}
+
+	digits = (char *)malloc(sizeof(char) * (digitCount + 1));
+
+	for (i = 0; i <= digitCount; i++)
+	{
+		digits[i] = (char)(num % 10 + '0');
+		num /= 10;
+	}
+
+	for (j = digitCount - 1; j >= 0; j--)
+	{
+		putchar(digits[j]);
+		count++;
+	}
+
+	return (count);
+}
+
+/**
  * _printf - Custom implementation of the printf function
  *
  * @format: Input string containing zero or more directives
@@ -11,6 +116,15 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
+	int i;
+
+	print_func_t methods[] = {
+	    {'c', print_char},
+	    {'s', print_string},
+	    {'%', print_percent},
+	    {'d', print_int},
+	    {'i', print_int},
+	    {'\0', NULL}};
 
 	va_start(args, format);
 
@@ -20,64 +134,12 @@ int _printf(const char *format, ...)
 		{
 			format++;
 
-			if (*format == 'c')
+			for (i = 0; methods[i].type != '\0'; i++)
 			{
-				char c = (char)va_arg(args, int);
-				putchar(c);
-				count++;
-			}
-
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-
-				while (*s != '\0')
+				if (methods[i].type == *format)
 				{
-					putchar(*s);
-					s++;
-					count++;
-				}
-			}
-
-			else if (*format == '%')
-			{
-				putchar('%');
-				count++;
-			}
-
-			else if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(args, int);
-				int digitCount = 0;
-				int temp, i, j;
-				char *digits;
-
-				if (num < 0)
-				{
-					putchar('-');
-					count++;
-					num = -num;
-				}
-
-				temp = num;
-				while (temp != 0)
-				{
-					digitCount++;
-					temp /= 10;
-				}
-
-				digits = (char *)malloc(sizeof(char) * (digitCount + 1));
-
-				for (i = 0; i <= digitCount; i++)
-				{
-					digits[i] = (char)(num % 10 + '0');
-					num /= 10;
-				}
-
-				for (j = digitCount - 1; j >= 0; j--)
-				{
-					putchar(digits[j]);
-					count++;
+					count += methods[i].execute(args);
+					break;
 				}
 			}
 		}
