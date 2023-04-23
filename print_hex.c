2053e38189f546d7a33e15e12af54a7ae5252994
Prosper Emebo <emeboprosper@gmail.com>
2023-04-23 01:45:05 +0100

    New Push

--- print_hex.c
+++ print_hex.c
@@ -9,43 +9,43 @@
  */
 int print_hex(va_list args, int uppercase)
 {
-        unsigned int num = va_arg(args, unsigned int);
-        char hex_digits[] = "0123456789abcdef";
-        int digitCount = 0;
-        int count = 0;
-        int temp, i, j;
-        char *digits;
+	unsigned int num = va_arg(args, unsigned int);
+	char hex_digits[] = "0123456789abcdef";
+	int digitCount = 0;
+	int count = 0;
+	int temp, i, j;
+	char *digits;
 
-        temp = num;
-        while (temp != 0)
-        {
-                digitCount++;
-                temp /= 16;
-        }
-        digits = (char *)malloc(sizeof(char) * (digitCount + 1));
-        for (i = 0; i <= digitCount; i++)
-        {
-                digits[i] = hex_digits[num % 16];
-                num /= 16;
-        }
-        digits[digitCount] = '\0';
-        if (uppercase)
-        {
-                for (j = digitCount - 1; j >= 0; j--)
-                {
-                        putchar(toupper(digits[j]));
-                        count++;
-                }
-        }
-        else
-        {
-                for (j = digitCount - 1; j >= 0; j--)
-                {
-                        putchar(digits[j]);
-                        count++;
-                }
-        }
-        return (count);
+	temp = num;
+	while (temp != 0)
+	{
+		digitCount++;
+		temp /= 16;
+	}
+	digits = (char *)malloc(sizeof(char) * (digitCount + 1));
+	for (i = 0; i <= digitCount; i++)
+	{
+		digits[i] = hex_digits[num % 16];
+		num /= 16;
+	}
+	digits[digitCount] = '\0';
+	if (uppercase)
+	{
+		for (j = digitCount - 1; j >= 0; j--)
+		{
+			putchar(toupper(digits[j]));
+			count++;
+		}
+	}
+	else
+	{
+		for (j = digitCount - 1; j >= 0; j--)
+		{
+			putchar(digits[j]);
+			count++;
+		}
+	}
+	return (count);
 }
 
 /**
@@ -56,7 +56,7 @@ int print_hex(va_list args, int uppercase)
  */
 int print_hex_lower(va_list args)
 {
-        return (print_hex(args, 0));
+	return (print_hex(args, 0));
 }
 
 /**
@@ -67,5 +67,5 @@ int print_hex_lower(va_list args)
  */
 int print_hex_upper(va_list args)
 {
-        return (print_hex(args, 1));
+	return (print_hex(args, 1));
 }
